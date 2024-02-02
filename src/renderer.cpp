3190d5ad96499dc30c0b704516cfef7ff9203eee
447721326@qq.com <72556475+talentstream@users.noreply.github.com>
2024-02-02 12:14:32 +0800

    feat: add parallel in renderer

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -9,6 +9,7 @@
 #include "util/parser.h"
 #include "util/bitmap.h"
 #include "util/timer.h"
+#include "util/parallel.h"
 #include <print>
 #include <ranges>
 
@@ -29,16 +30,33 @@ NAMESPACE_BEGIN
         const Camera *pCamera = pScene->GetCamera();
         const Integrator *pIntegrator = pScene->GetIntegrator();
         const Aggregate *pAggregate = pScene->GetAggregate();
+        Integer tileSizeX{8}, tileSizeY{8};
+        Integer tileHeight = std::floor(static_cast<Float>(400) / static_cast<Float>(tileSizeY));
+        Integer tileWidth = std::floor(static_cast<Float>(400) / static_cast<Float>(tileSizeX));
+
+        auto RenderBlock = [&](Integer tileX, Integer tileY) {
+            auto beginY = tileY * tileHeight;
+            auto endY = (tileY + 1) * tileHeight;
+            endY = endY > 400 ? 400 : endY;
+            auto beginX = tileX * tileWidth;
+            auto endX = (tileX + 1) * tileWidth;
+            endX = endX > 400 ? 400 : endX;
+
+            // write reverse-y
+            for (const Integer y: std::views::iota(beginY, endY)) {
+                auto index = (400 - y - 1) * 400;
+                for (const Integer x: std::views::iota(beginX, endX)) {
+                    Ray ray = pCamera->GenerateRay(Point2f(x, y));
+                    Color3f L = pIntegrator->Li(ray, *pAggregate);
+                    mFramebuffer[index + x] += L;
+                }
+            }
+        };
 
         if(mCurrentSpp < pScene->GetSpp())
         {
             Timer timer;
-            for (int i = 0; i < 400; i++) {
-                for (int j = 0; j < 400; j++) {
-                    Ray ray = pCamera->GenerateRay(Point2f(j, i));
-                    mFramebuffer[i * 400 + j] += pIntegrator->Li(ray, *pAggregate, 0);
-                }
-            }
+            Parallel::For2D(0,tileSizeX, 0,tileSizeY, RenderBlock);
             mCurrentSpp++;
             std::print("SPP: {} - ", mCurrentSpp);
             timer.PrintElapsedMillSec();
