f8169cbcc728d16aab311b70fa253d6f88dca02f
447721326@qq.com <72556475+talentstream@users.noreply.github.com>
2024-02-10 22:36:58 +0800

    refactor: add aggregate in integrator

--- inc/base/integrator.h
+++ inc/base/integrator.h
@@ -6,6 +6,7 @@
 
 #include "core/object.h"
 #include "base/aggregate.h"
+#include <print>
 
 NAMESPACE_BEGIN
 
@@ -13,11 +14,38 @@ class Integrator : public Object {
 public:
     virtual ~Integrator() = default;
 
-    virtual Color3f Li(const Ray &ray, const Aggregate &aggregate) const = 0;
+//    virtual void Render() const final;
+
+    [[nodiscard]] virtual Color3f Li(const Ray &ray, const Aggregate &aggregate) const = 0;
+
+    virtual void AddChild(Object *child) {
+        std::print("Integrator::AddChild\n");
+        switch (child->GetClassType()) {
+            case EClassType::EAggregate:
+                pAggregate = dynamic_cast<Aggregate *>(child);
+                break;
+            default:
+                break;
+        }
+    }
+
+    virtual void Initialize() {
+        if (pAggregate == nullptr) {
+            throw std::runtime_error("Integrator Need Aggregate!");
+        }
+    }
 
     [[nodiscard]] EClassType GetClassType() const override {
         return EClassType::EIntegrator;
     }
+
+protected:
+    Aggregate *pAggregate{nullptr};
+    // std::vector<Light> mLights;
+
+    // Camera *pCamera{nullptr};
+    // Sampler *pSampler{nullptr};
 };
+
 
 NAMESPACE_END
--- src/integrator/normal.cpp
+++ src/integrator/normal.cpp
@@ -13,7 +13,7 @@ public:
         PRINT_DEBUG_INFO("Integrator", "normal")
     }
 
-    Color3f Li(const Ray &ray, const Aggregate &aggregate) const override {
+    [[nodiscard]] Color3f Li(const Ray &ray, const Aggregate &aggregate) const override {
         Interaction i;
         if (!aggregate.Intersect(ray, i)) {
             return {0.5f, 0.7f, 1.0f};
@@ -21,6 +21,7 @@ public:
 
         return Color3f{i.n.x + 1, i.n.y + 1, i.n.z + 1} * 0.5f;
     }
+
 };
 
 REGISTER_CLASS(NormalIntegrator, "normal")
