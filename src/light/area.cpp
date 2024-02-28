86fa657d588a899eb74ccdd0c3a0bcf8d17159de
talentstream <72556475+talentstream@users.noreply.github.com>
2024-02-28 20:04:19 +0800

    feat: add area light interface

---
+++ src/light/area.cpp
@@ -0,0 +1,17 @@
+//
+// Created by talentstream on 2024/2/28.
+//
+
+#include "base/light.h"
+
+NAMESPACE_BEGIN
+class AreaLight : public Light {
+public:
+    explicit AreaLight(const PropertyList &propList) {
+    }
+
+private:
+
+};
+
+NAMESPACE_END
