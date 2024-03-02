0ddb385274b28326d3f6ad18c6119d51c2b33f48
talentstream <72556475+talentstream@users.noreply.github.com>
2024-03-02 16:05:36 +0800

    refactor: roughdiffuse

--- src/bxdf/roughdiffuse.cpp
+++ src/bxdf/roughdiffuse.cpp
@@ -42,7 +42,7 @@ public:
             maxCos = Max(0, dCos);
         }
         Float sinAlpha, tanBeta;
-        if(Frame::AbsCosTheta(wi) > Frame::AbsCosTheta(wo)) {
+        if (Frame::AbsCosTheta(wi) > Frame::AbsCosTheta(wo)) {
             sinAlpha = sinThetaO;
             tanBeta = sinThetaI / Frame::AbsCosTheta(wi);
         } else {
@@ -51,6 +51,38 @@ public:
         }
 
         return mAlbedo * InvPi * (A + B * maxCos * sinAlpha * tanBeta);
+    }
+
+    Color3f
+    Eval(const BxDFSampleRecord &bRec) const override {
+        if (Frame::CosTheta(bRec.wo) <= 0 ||
+            Frame::CosTheta(bRec.wi) <= 0) {
+            return {0.f};
+        }
+
+        Float sinThetaI = Frame::SinTheta(bRec.wi);
+        Float sinThetaO = Frame::SinTheta(bRec.wo);
+
+        Float maxCos{0};
+        if (sinThetaI > Epsilon && sinThetaO > Epsilon) {
+            Float sinPhiI = Frame::SinPhi(bRec.wi),
+                    cosPhiI = Frame::CosPhi(bRec.wi),
+                    sinPhiO = Frame::SinPhi(bRec.wo),
+                    cosPhiO = Frame::CosPhi(bRec.wo);
+            Float dCos = cosPhiI * cosPhiO + sinPhiI * sinPhiO;
+            maxCos = Max(0, dCos);
+        }
+        Float sinAlpha, tanBeta;
+        if (Frame::AbsCosTheta(bRec.wi) > Frame::AbsCosTheta(bRec.wo)) {
+            sinAlpha = sinThetaO;
+            tanBeta = sinThetaI / Frame::AbsCosTheta(bRec.wi);
+        } else {
+            sinAlpha = sinThetaI;
+            tanBeta = sinThetaO / Frame::AbsCosTheta(bRec.wo);
+        }
+
+        return mAlbedo * InvPi * (A + B * maxCos * sinAlpha * tanBeta);
+
     }
 
     Float
@@ -63,6 +95,15 @@ public:
         return Warp::SquareToCosineHemispherePdf(wo);
     }
 
+    Float
+    pdf(const BxDFSampleRecord &bRec) const override {
+        if (Frame::CosTheta(bRec.wo) <= 0 ||
+            Frame::CosTheta(bRec.wi) <= 0) {
+            return 0.f;
+        }
+        return Warp::SquareToCosineHemispherePdf(bRec.wo);
+    }
+
     std::optional<Color3f>
     SampleF(const SurfaceInteraction &si, const Vector3f wo, Vector3f &wi, Point2f sample) const override {
         if (Frame::CosTheta(wo) < 0) return std::nullopt;
@@ -70,6 +111,17 @@ public:
         wi = Warp::SquareToCosineHemisphere(sample);
         // (brdf / pdf) * cos = [(albedo / pi) / (cos / pi)] * cos
         return mAlbedo;
+    }
+
+    Color3f
+    Sample(BxDFSampleRecord &bRec, Float &pdf, const Point2f &sample) const override {
+        if (Frame::CosTheta(bRec.wi) <= 0) return {0.f};
+
+        bRec.wo = Warp::SquareToCosineHemisphere(sample);
+
+        pdf = Warp::SquareToCosineHemispherePdf(bRec.wo);
+
+        return Eval(bRec) / pdf;
     }
 
     BxDFFlag Flag() const override {
