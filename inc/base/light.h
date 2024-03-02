09eb64c576ab4225b7e5e3e52065cdc2aa617daf
talentstream <72556475+talentstream@users.noreply.github.com>
2024-03-02 22:37:18 +0800

    feat: add light type

--- inc/base/light.h
+++ inc/base/light.h
@@ -9,6 +9,26 @@
 
 NAMESPACE_BEGIN
 
+enum class LightType {
+    EDeltaPosition,
+    EDeltaDirection,
+    EArea,
+    EInfinite
+};
+
+inline Boolean IsDeltaLight(LightType type) {
+    return (type == LightType::EDeltaPosition ||
+            type == LightType::EDeltaDirection);
+}
+
+inline Boolean IsAreaLight(LightType type) {
+    return (type == LightType::EArea);
+}
+
+inline Boolean IsInfiniteLight(LightType type) {
+    return (type == LightType::EInfinite);
+}
+
 class Light : public Object {
 public:
     virtual ~Light() = default;
@@ -24,7 +44,8 @@ public:
         return {0};
     }
 
-    virtual Color3f SampleLi(const SurfaceInteraction &si, Vector3f &wi, Point2f &sample) const = 0;
+    virtual Color3f
+    SampleLi(const SurfaceInteraction &si, Vector3f &wi, Point2f &sample) const = 0;
 
     [[nodiscard]] EClassType GetClassType() const override {
         return EClassType::ELight;
