84ba8c006a1a48e620982ea054a3ac0266af7976
talentstream <72556475+talentstream@users.noreply.github.com>
2024-03-04 16:03:14 +0800

    feat: add new aggregate interface

--- inc/base/aggregate.h
+++ inc/base/aggregate.h
@@ -6,12 +6,14 @@
 
 #include "core/object.h"
 #include "core/interaction.h"
+#include "base/shape.h"
 
 NAMESPACE_BEGIN
 
 class Aggregate : public Object {
 public:
-    virtual ~Aggregate() = default;
+    virtual
+    ~Aggregate() = default;
 
     virtual Boolean
     Intersect(const Ray &ray, SurfaceInteraction &interaction) const = 0;
@@ -19,9 +21,48 @@ public:
     virtual Boolean
     UnOccluded(const Ray &ray) const = 0;
 
-    [[nodiscard]] EClassType GetClassType() const override {
+    EClassType
+    GetClassType() const override {
         return EClassType::EAggregate;
     }
+};
+
+class aggregate : public Object {
+public:
+    virtual
+    ~aggregate() = default;
+
+    virtual Boolean
+    Intersect(const Ray &ray, SurfaceInteraction &interaction) const = 0;
+
+    virtual Boolean
+    UnOccluded(const Ray &ray) const = 0;
+
+    EClassType
+    GetClassType() const override {
+        return EClassType::EAggregate;
+    }
+
+    void
+    AddChild(Object *child) override;
+
+protected:
+    void
+    AddShape(Shape *shape);
+
+    virtual UInt32
+    FindShape(UInt32 idx) const;
+
+    virtual Bound3f
+    FindShapeBoundingBox(UInt32 idx) const;
+
+    virtual Point3f
+    FindShapeCentroid(UInt32 idx) const;
+
+    std::vector<Shape *> mShapes;
+    std::vector<UInt32> mShapeOffset;
+    std::vector<UInt32> mShapeIndices;
+    Bound3f mBBox;
 };
 
 NAMESPACE_END
