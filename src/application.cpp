e41a40e13887b9e48d035700f802073bf6a9ea40
talentstream <72556475+talentstream@users.noreply.github.com>
2024-03-08 10:47:07 +0800

    delete useless xml & organize project structure

--- src/application.cpp
+++ src/application.cpp
@@ -20,7 +20,7 @@ Integer windowWidth{};
 Integer windowHeight{};
 
 Application::Application() {
-    Object *obj = LoadSceneXML("../cornellbox.xml");
+    Object *obj = LoadSceneXML("../res/test/light.xml");
     pScene = std::unique_ptr<Scene>(dynamic_cast<Scene *>(obj));
     windowWidth = pScene->GetIntegrator()->GetCamera()->GetFilm()->width;
     windowHeight = pScene->GetIntegrator()->GetCamera()->GetFilm()->height;
