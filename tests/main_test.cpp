97b06247c61ec787c8fbc84721523992fdc808e3
talentstream <72556475+talentstream@users.noreply.github.com>
2024-03-15 11:45:44 +0800

    test: add test

---
+++ tests/main_test.cpp
@@ -0,0 +1,7 @@
+//
+// Created by talentstream on 2024/3/15.
+//
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <doctest.h>
--- tests/property_test.cpp
+++ tests/property_test.cpp
@@ -7,10 +7,17 @@
 
 #include <core/propertylist.h>
 
-TEST_CASE("test property list") {
+TEST_CASE("Test Int") {
     USING_NAMESPACE
     PropertyList list;
     list.SetInt("int", 1);
     CHECK(list.GetInt("default", {}) == 0);
     CHECK(list.GetInt("int", {}) == 1);
+    list.SetInt("intf", 1.5);
+    CHECK(list.GetInt("intf", {}) == 1);
+}
+
+TEST_CASE("TEST Float") {
+    USING_NAMESPACE
+
 }
